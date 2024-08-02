9e853b30f4ccbd8291bbefc328ea7ab48eec7553
Sreemayi Billa <158159700+sreemayi-25@users.noreply.github.com>
2024-08-02 09:27:10 +0530

    Create Word Order.py

---
+++ Collections/Word Order.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter
+n=int(input())
+l=[]
+for i in range(n):
+    l.append(input())
+c=Counter(l)
+print(len(c))
+print(*c.values())
