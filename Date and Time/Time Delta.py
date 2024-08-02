004e925b83730a666496de6fe34df4d676299875
Sreemayi Billa <158159700+sreemayi-25@users.noreply.github.com>
2024-08-02 09:30:23 +0530

    Update Time Delta.py

--- Date and Time/Time Delta.py
+++ Date and Time/Time Delta.py
@@ -1,5 +1,9 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import calendar
-m,d,y=[int(x) for x in input().split(" ")]
-days = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY","SUNDAY"]
-print(days[calendar.weekday(y, m, d)])
+from datetime import datetime 
+# Complete the time_delta function below.
+t=int(input())
+for i in range(t):
+    t1=input()
+    t2=input()
+    d1=datetime.strptime(t1, '%a %d %b %Y %H:%M:%S %z')
+    d2=datetime.strptime(t2, '%a %d %b %Y %H:%M:%S %z')
+    print(int(abs(d1-d2).total_seconds()))
